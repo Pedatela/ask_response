8a0641782c5b03700a2e41988963e9732d7d8f07
Pedatela <lucaspedatela@gmail.com>
2022-03-22 14:28:33 -0300

    Ajustando rotas

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -2,4 +2,13 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def index
     @admins = Admin.all
   end
+
+  def edit
+
+  end
+
+  def show
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   namespace :admins_backoffice do
-    get 'admins/index'
+    resources :admins, only: [:index]
+    get 'welcome/index'
   end
-  devise_for :users
   namespace :site do
     get 'welcome/index'
   end
   namespace :users_backoffice do
     get 'welcome/index'
   end
-  namespace :admins_backoffice do
-    get 'welcome/index'
-  end
+  
+  devise_for :users
   devise_for :admins
   get 'site/welcome/index'
   root 'site/welcome#index'
