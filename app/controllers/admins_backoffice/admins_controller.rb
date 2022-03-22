6e531265581bd5c7694d2414b864e1bdfa4d5424
Pedatela <lucaspedatela@gmail.com>
2022-03-22 15:49:51 -0300

    Finalizando formulario

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,10 +1,20 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def index
-    @admins = Admin.all
+    @admins = Admin.all.order(:id)
   end
 
   def edit
     @admin = Admin.find(params[:id])
+  end
+
+  def update
+    admin = Admin.find(params[:id])
+    params_admin = params.require(:admin).permit(:email, :password, :password_connfirmation)
+    if admin.update(params_admin)
+      redirect_to admins_backoffice_admins_path, notice: "Adm atualizado com Sucesso"
+    else
+      render :edit
+    end
   end
 
   def show
