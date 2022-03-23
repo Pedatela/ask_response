1b9a33390e17bace30517373037bebc2254c8510
Pedatela <lucaspedatela@gmail.com>
2022-03-23 17:03:51 -0300

    Criando controller e rotas das perguntas

---
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -0,0 +1,53 @@
+class AdminsBackoffice::QuestionsController < AdminsBackofficeController
+    before_action :set_question, only: [:update, :edit, :destroy]
+  
+    def index
+        @questions = Question.all.order(:description).page(params[:page])
+    end
+
+    def edit
+    end
+
+    def update
+        if @question.update(params_question)
+            redirect_to admins_backoffice_questions_path, notice: "Perguntas atualizada com Sucesso"
+        else
+            render :edit
+        end
+    end
+
+    def show
+        
+    end
+
+    def new
+        @question = Question.new
+    end
+
+    def create
+        @question = Question.new(params_question)
+        if @question.save()
+            redirect_to admins_backoffice_questions_path, notice: "Perguntas cadastrada com Sucesso"
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        if @question.destroy
+            redirect_to admins_backoffice_questions_path, notice: "Perguntas excluida com Sucesso"
+        else
+            render :index
+        end
+    end
+
+    private
+
+    def params_question
+        params_question = params.require(:question).permit(:description, :subject_id)
+    end
+
+    def set_question
+        @question = Question.find(params[:id])
+    end
+end
